d4c43c6039dee1c4f98ffeab24c289a91e44f24b
Alison da Rocha <alison.sr69@gmail.com>
2022-09-15 01:57:15 -0300

    initial commit, criação do projeto e instalação dos packages

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Q.A.__social_network.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+}
